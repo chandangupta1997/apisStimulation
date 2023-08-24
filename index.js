20041be76b6c3aeda6ef466e602d6d9c8b172e92
chandangupta1997 <chandan.gupta@cyberange.io>
2023-08-24 19:55:25 +0530

    second commit

--- index.js
+++ index.js
@@ -1,30 +1,103 @@
-const express = require('express');
-const Chance = require('chance');
+
+
+
+
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
-const chance = new Chance();
+const airlines = [
 
-// Generate random flight data for UAE airport
-function generateRandomFlight() {
-    return {
-        flightNumber: chance.string({ pool: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', length: 6 }),
-        airline: chance.company(),
-        destination: chance.city(),
-        departureTime: chance.hour({ twentyfour: true }) + ':' + chance.minute(),
-        gate: chance.integer({ min: 1, max: 10 }),
-    };
-}
-
-// Define a route to get random flight data
-app.get('/api/flights', (req, res) => {
-    const numberOfFlights = chance.integer({ min: 5, max: 10 });
-    const flights = Array.from({ length: numberOfFlights }, generateRandomFlight);
-    res.json(flights);
+    {
+      Name: "Qatar Airways ",
+      Image:
+        "https://www.qatarairways.com/content/dam/qatar/images/qr-official-logo.png",
+    },
+    {
+      Name: "Singapore Airlines",
+      Image:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Singapore_Airlines_Logo_2.svg/1200px-Singapore_Airlines_Logo_2.svg.png",
+    },
+  
+    {
+      Name:"Emirates",
+      Image:"https://c.ekstatic.net/ecl/logos/emirates/emirates-logo-badge.png"
+  
+    },
+    {
+      Name: "Delta AirLines",
+      Image:"https://1000logos.net/wp-content/uploads/2017/09/Delta-Air-Lines-Logo.png"
+    }
+  
+    ,
+  
+    {
+      Name:"Air India",
+      Image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZYtYukk0PWPPcqOeIAwY_CyDLJP690gizZg&usqp=CAU"
+    },
+   
+    ,
+    {
+      Name:"All Nippon Airways ",
+      Image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/All_Nippon_Airways_Logo.svg/2560px-All_Nippon_Airways_Logo.svg.png"
+    },
+    {
+      Name:"Lufthansa",
+      Image:"https://www.lufthansa.com/etc/designs/dcep/logo-lh-og.jpg"
+    },
+    {
+      Name:"United Airlines",
+      Image:"https://1000logos.net/wp-content/uploads/2017/06/United-Airlines-Logo.png"
+  
+    },
+    {
+      Name:"Ethihad Airways",
+      Image:"http://t3.gstatic.com/images?q=tbn:ANd9GcRO96TSkb8Tv7BsoWd38BnmzqIuulhyMwWt_WDjCGza3OZr-qXo"
+    },
+    {
+      Name:"Qantas Airways",
+      Image:"https://1000logos.net/wp-content/uploads/2017/05/Qantas-Logo.png"
+    }
+    
+  ];
+  
+  function generateRandomFlight() {
+    const flights = [];
+    const availableIndexes = airlines.map((_, index) => index);
+  
+    while (flights.length < 10 && availableIndexes.length > 0) {
+      const randomIndex = Math.floor(Math.random() * availableIndexes.length);
+      const selectedIndex = availableIndexes[randomIndex];
+  
+      availableIndexes.splice(randomIndex, 1);
+      const randomAirline = airlines[selectedIndex];
+  
+      flights.push({
+        flightName: randomAirline.Name,
+        image: randomAirline.Image,
+        carrier: randomAirline.Name,
+        origin: "Dubai",
+        via: "-",
+        time: `${Math.floor(Math.random() * 24)}:${Math.floor(Math.random() * 60)}`,
+        terminal: Math.floor(Math.random() * 3) + 1,
+        belt: Math.floor(Math.random() * 20) + 1,
+        status: ["On Time", "Delayed", "Boarding"][Math.floor(Math.random() * 3)],
+      });
+    }
+  
+    return flights;
+  }
+app.get("/api/flightsDeparture", function(req, res) {
+  const flights = generateRandomFlight();
+  res.json(flights);
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.get("/api/flightsArrival", function(req, res) {
+  const flights = generateRandomFlight();
+  res.json(flights);
+});
+
+app.listen(port, function() {
+  console.log("Server is running on port " + port);
 });
